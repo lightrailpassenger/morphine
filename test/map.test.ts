ce311836a8b312be469edd859f8988a08ee1c9d0
Tony Chow <tonychow.0929@icloud.com>
2021-11-26 15:52:47 +0800

    Split test cases into multiple files

--- test/map.test.ts
+++ test/map.test.ts
@@ -1,26 +1,7 @@
 import { assertEquals } from "https://deno.land/std@0.115.1/testing/asserts.ts";
 import { delay } from "https://deno.land/std@0.115.1/async/delay.ts";
 
-import Morphine from "./mod.ts";
-
-Deno.test("Entries", async () => {
-  const it = new Morphine<number>((async function* () {
-    yield 5;
-    yield 6;
-    yield 7;
-    yield 8;
-  })());
-  const results: Array<[number, number]> = [];
-  for await (const item of it.entries()) {
-    results.push(item);
-  }
-  assertEquals(results, [
-    [0, 5],
-    [1, 6],
-    [2, 7],
-    [3, 8],
-  ]);
-});
+import Morphine from "../mod.ts";
 
 Deno.test("Map", async () => {
   const it = new Morphine<string>((async function* () {
